79f76aba881164a667cb793838fa6f48cc078b74
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-09-25 11:22:57 -0500

    problem 5

--- problem5.js
+++ problem5.js
@@ -1,8 +1,10 @@
 // Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
-const is_leapyear = year => new Date(year, 1, 29).getMonth() === 1;
-console.log(is_leapyear(2016));
-console.log(is_leapyear(2000));
-console.log(is_leapyear(1700));
-console.log(is_leapyear(1800));
-console.log(is_leapyear(100));
+function leapyear(year) {
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+  }
+  console.log(leapyear(2016));
+  console.log(leapyear(2000));
+  console.log(leapyear(1700));
+  console.log(leapyear(1800));
+  console.log(leapyear(100));
