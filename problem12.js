f10ed6b293d6457c356421bd343d444df18c1f51
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-10-03 12:00:34 -0500

    problem 12

---
+++ problem12.js
@@ -0,0 +1,17 @@
+//Write a JavaScript program to check from two given integers, whether one is positive and another one is negative.JavaScript
+
+function positive_negative(x, y)
+{
+  if ((x < 0 && y > 0) || x > 0 && y < 0) 
+  {
+    return true;
+  }
+  else 
+  {
+    return false;
+  }
+}
+console.log(positive_negative(2, 2));
+console.log(positive_negative(-2, 2));
+console.log(positive_negative(2, -2));
+console.log(positive_negative(-2, -2));
